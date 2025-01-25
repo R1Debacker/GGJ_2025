241acce81ee138e0fb9107db7ea247ce37b8e23e
drazicmartin <41931010+drazicmartin@users.noreply.github.com>
2025-01-25 03:51:18 +0100

    fix: adapt boostzone to Player

--- ggj2025/HugoLeBo/Scripts/BoostZone.gd
+++ ggj2025/HugoLeBo/Scripts/BoostZone.gd
@@ -1,12 +1,24 @@
 extends Area2D
 
+@export var speed : float = 10
+
+var players = []
+
+func _physics_process(delta: float) -> void:
+	for player in players:
+		player.env_velocity = Vector2(cos(rotation), sin(rotation)) * speed
+
 func _on_body_entered(body: Node2D) -> void:
 	if (body is Player):
-		body.factor=1
-		body.direction =Vector2(cos(rotation), sin(rotation))
+		if not players.has(body):
+			players.append(body)
 	pass # Replace with function body.
 
 func _on_body_exited(body: Node2D) -> void:
 	if (body is Player):
-		body.factor=0.5
+		if players.has(body):
+			var idx = players.find(body)
+			if idx != -1:
+				players.remove_at(idx)
+	print(players)
 	pass # Replace with function body.
