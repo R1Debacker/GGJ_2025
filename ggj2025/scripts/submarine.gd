a09aa9c454848bb73c6c86285b337ad3d661afd9
Hugodu34 <hugo.gelineau@outlook.fr>
2025-01-25 18:57:22 +0100

    Ajout son filling Submarine

--- ggj2025/scripts/submarine.gd
+++ ggj2025/scripts/submarine.gd
@@ -2,25 +2,28 @@ extends Node2D
 @onready var texture_progress_bar: TextureProgressBar = $TextureProgressBar
 @export var filling_speed := 50.0
 @export var target_oxygen_level := 1000.0
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 var current_oxygen_level := 0.0
 var current_shrimp : CharacterBody2D = null
 
 func _ready() -> void:
 	$AnimationPlayer.play("vertical_animation")
-
+	
 func _process(delta: float) -> void:
 	if current_shrimp != null:
+		if audio_stream_player_2d.is_playing() == false:
+			audio_stream_player_2d.play()
 		current_oxygen_level += filling_speed * delta
 		if current_oxygen_level > target_oxygen_level:
 			_victory()
+	else :
+		audio_stream_player_2d.stop()
 	texture_progress_bar.value=current_oxygen_level
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D && current_shrimp == null:
 		current_shrimp = body
-
-
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body is CharacterBody2D && body == current_shrimp: 
