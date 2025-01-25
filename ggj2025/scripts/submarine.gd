90e7e574b2f63c606ac958b14fc46e58f89169c7
R1Debacker <erwann.debacker@gmail.com>
2025-01-25 23:36:09 +0100

    feat : big fish sa mère

--- ggj2025/scripts/submarine.gd
+++ ggj2025/scripts/submarine.gd
@@ -6,23 +6,30 @@ extends Node2D
 
 var current_oxygen_level := 0.0
 var current_shrimp : CharacterBody2D = null
+var victory := false
 
 func _ready() -> void:
 	$AnimationPlayer.play("vertical_animation")
 	
 func _process(delta: float) -> void:
-	if current_shrimp != null:
+	if current_shrimp != null && current_shrimp.has_bubble && current_shrimp.bubble.air_volume > 0.0:
 		if audio_stream_player_2d.is_playing() == false:
-			audio_stream_player_2d.play()
-		current_oxygen_level += filling_speed * delta
+				audio_stream_player_2d.play()
+		var oxygen_value := filling_speed * delta
+		if oxygen_value < current_shrimp.bubble.air_volume:
+			current_oxygen_level += oxygen_value
+			current_shrimp.bubble.add_volume(-oxygen_value)
+		else:
+			current_shrimp.bubble.set_volume(0.0)
+			current_oxygen_level += current_shrimp.bubble.air_volume
 		if current_oxygen_level > target_oxygen_level:
 			_victory()
 	else :
 		audio_stream_player_2d.stop()
-	texture_progress_bar.value=current_oxygen_level
+	texture_progress_bar.value= (current_oxygen_level*1.0)/(target_oxygen_level*1.0)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body is CharacterBody2D && current_shrimp == null:
+	if body is Player && current_shrimp == null:
 		current_shrimp = body
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
@@ -32,7 +39,8 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 
 func _victory():
 	print("victory")
-	pass
+	victory = true
+	queue_free()
 
 func _find_shrimp_in_area():
 	if $Sprite2D/Area2D.has_overlapping_bodies():
