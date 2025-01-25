994f4f9b59460e53420a0c3a2064540dc85a40ad
R1Debacker <erwann.debacker@gmail.com>
2025-01-25 13:12:52 +0100

    fix : ennemy script error

--- ggj2025/scripts/ennemy.gd
+++ ggj2025/scripts/ennemy.gd
@@ -5,7 +5,7 @@ var start_position := Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
