16d5e014c16b4dd24b2381d17725ce4c5e98de07
Hugodu34 <hugo.gelineau@outlook.fr>
2025-01-26 00:59:10 +0100

    Balancing Sounds and Gouvernment

--- ggj2025/autoloads/Game.gd
+++ ggj2025/autoloads/Game.gd
@@ -1,10 +1,17 @@
 extends Node
-
+@onready var timer: Timer = $Timer
 @onready var menu_button_sound: AudioStreamPlayer2D = $menu_button_sound
 @onready var bubble_pop: AudioStreamPlayer2D = $BubblePop
+@onready var back_sound: AudioStreamPlayer2D = $BackSound
+@onready var ellie: AudioStreamPlayer2D = $Ellie
 
 const BUBBLE = preload("res://scenes/bubble.tscn")
 const MOVING_BUBBLE = preload("res://scenes/moving_bubble.tscn")
 
 var nb_players := 0
 var target_oxygen_level := 1000.0
+
+
+func _on_timer_timeout() -> void:
+	ellie.play()
+	pass # Replace with function body.
