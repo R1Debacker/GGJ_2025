95f465b91ed9edcb6ee1bccd95b9c714417ef866
Maxime SEIGNEUR <81438664+SeigneurM@users.noreply.github.com>
2025-01-25 16:50:59 +0100

    Enemy poisson t'y as capté

---
+++ ggj2025/scripts/enemy.gd
@@ -0,0 +1,63 @@
+extends Node2D
+
+@export var damage_amount := 50.0
+@export var speed_factor := 0.6
+@export var distance_to_touch := 25.0
+var enemy_speed := 300.0
+var current_target : Node2D = null
+@onready var following_time: Timer = $FollowingTime
+@onready var enemy_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+enum STATE{
+	IDLE,
+	FOLLOW
+}
+
+var state :=STATE.IDLE
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	enemy_sprite.play('Idle')
+	pass # Replace with function body.
+
+func _on_body_entered(body: Node) -> void:
+	
+	if body is Player and state==STATE.FOLLOW :
+		state=STATE.IDLE
+		current_target = null
+
+func _on_radar_area_body_entered(body: Node2D) -> void:
+
+	if body is Player and state==STATE.IDLE :
+
+		state=STATE.FOLLOW
+		current_target=body
+		enemy_speed=speed_factor*current_target.player_speed*0.7
+		following_time.start()
+
+		
+func _follow(delta: float):
+	
+	look_at(current_target.global_position)
+	enemy_sprite.flip_v = current_target.global_position.x<global_position.x
+	global_position=global_position.move_toward(current_target.global_position,delta*enemy_speed)
+	if $Beak.global_position.distance_to(current_target.global_position)<3.0:
+		_on_body_entered(current_target)
+	
+func _physics_process(delta: float) -> void:
+
+	match(state):
+		STATE.FOLLOW:
+			_follow(delta)
+		STATE.IDLE:
+			pass
+			#se balader
+			
+			
+
+func _on_following_time_timeout() -> void:
+	
+	if state==STATE.FOLLOW:
+		state=STATE.IDLE
+		current_target = null
